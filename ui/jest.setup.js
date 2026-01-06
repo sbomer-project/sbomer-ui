06ec601c2dde3429c70e10440b285aa467389009
Tomas Havlicek <thavlice@ibm.com>
2026-01-06 18:08:24 +0100

    test: fix test suite, add basic tests

--- ui/jest.setup.js
+++ ui/jest.setup.js
@@ -3,3 +3,18 @@ const { TextEncoder, TextDecoder } = require('util');
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+// Mock window.matchMedia for Carbon Design System components
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
